de777ebbc9710987cc27f86ac724d0cb0d5b92e9
Eideren <contact@eideren.com>
2026-01-19 20:30:06 +0100

    fix: Workaround for unexpected RequiredMemberAttribute inserted on certain fields

--- Editor/RequiredMemberAttributeValidator.cs
+++ Editor/RequiredMemberAttributeValidator.cs
@@ -1,9 +1,21 @@
+using Sirenix.OdinInspector.Editor;
 using Sirenix.OdinInspector.Editor.Validation;
 
 [assembly: RegisterValidator(typeof(RequiredMemberAttributeValidator<>))]
 
-public class RequiredMemberAttributeValidator<T> : AttributeValidator<System.Runtime.CompilerServices.RequiredMemberAttribute, T>// where T : class
+public class RequiredMemberAttributeValidator<T> : AttributeValidator<System.Runtime.CompilerServices.RequiredMemberAttribute, T>
 {
+    public override bool CanValidateProperty(InspectorProperty property)
+    {
+        if (base.CanValidateProperty(property))
+        {
+            // Looks like odin has a bug where fields may receive a member required attribute even though they are not marked as such
+            return property.Info.GetAttribute<System.Runtime.CompilerServices.RequiredMemberAttribute>() is not null;
+        }
+
+        return false;
+    }
+
     protected override void Validate(ValidationResult result)
     {
         object? weakValue = Property.ValueEntry.WeakSmartValue;
--- RequiredWorkaround.cs
+++ RequiredWorkaround.cs
@@ -1,6 +1,6 @@
 namespace System.Runtime.CompilerServices
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
     public sealed class RequiredMemberAttribute : Attribute { }
 
     [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
