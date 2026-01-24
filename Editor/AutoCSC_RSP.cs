5ddfb99d47286b4b1ba1242c5702bc2a88178827
Eideren <contact@eideren.com>
2026-01-24 14:51:49 +0100

    fix: Generate csc.rsp only for writable sources

--- Editor/AutoCSC_RSP.cs
+++ Editor/AutoCSC_RSP.cs
@@ -19,53 +19,57 @@ namespace YNode.Editor
             {
                 try
                 {
-                    foreach (string asmdefPath in Directory.EnumerateFiles(Directory.GetParent(Application.dataPath)!.FullName, "*.asmdef", SearchOption.AllDirectories))
+                    foreach (var subDir in new[] { "Assets", "Packages" })
                     {
-                        if (Regex.Match(File.ReadAllText(asmdefPath), """\"name\"\s*:\s*\"([^"]*)""") is not { } m || !m.Success)
+                        var fullPath = Path.Combine(Directory.GetParent(Application.dataPath)!.FullName, subDir);
+                        foreach (string asmdefPath in Directory.EnumerateFiles(fullPath, "*.asmdef", SearchOption.AllDirectories))
                         {
-                            Debug.LogWarning($"Could not find assembly name in assembly definition at path {asmdefPath}");
-                            continue;
+                            if (Regex.Match(File.ReadAllText(asmdefPath), """\"name\"\s*:\s*\"([^"]*)""") is not { } m || !m.Success)
+                            {
+                                Debug.LogWarning($"Could not find assembly name in assembly definition at path {asmdefPath}");
+                                continue;
+                            }
+
+                            var csc = Path.Combine(Path.GetDirectoryName(asmdefPath), "csc.rsp");
+                            var cscMeta = Path.Combine(Path.GetDirectoryName(asmdefPath), "csc.rsp.meta");
+                            string content;
+                            try
+                            {
+                                content = File.ReadAllText(csc);
+                            }
+                            catch (Exception)
+                            {
+                                content = "";
+                            }
+
+                            var newContent = content;
+                            if (content.Contains("-langVersion:") == false)
+                                newContent = $"{newContent.Trim()} -langVersion:preview";
+
+                            if (content.Contains("-nullable") == false)
+                                newContent = $"{newContent.Trim()} -nullable";
+
+                            if (content.Contains("-doc:") == false)
+                                newContent = $"{newContent.Trim()} -doc:Library/StreamingAssets/{m.Groups[1]}.xml";
+
+                            if (content.Contains("-nowarn:") == false)
+                                newContent = $"{newContent.Trim()} -nowarn:1591";
+
+                            if (content == newContent)
+                                continue;
+
+                            File.WriteAllText(csc, newContent);
+
+                            if (File.Exists(cscMeta) == false)
+                                File.WriteAllText(cscMeta, @$"fileFormatVersion: 2
+    guid: {Guid.NewGuid().ToString().Replace("-", "")}
+    AssemblyDefinitionImporter:
+      externalObjects: {{}}
+      userData:
+      assetBundleName:
+      assetBundleVariant:
+    ");
                         }
-
-                        var csc = Path.Combine(Path.GetDirectoryName(asmdefPath), "csc.rsp");
-                        var cscMeta = Path.Combine(Path.GetDirectoryName(asmdefPath), "csc.rsp.meta");
-                        string content;
-                        try
-                        {
-                            content = File.ReadAllText(csc);
-                        }
-                        catch (Exception)
-                        {
-                            content = "";
-                        }
-
-                        var newContent = content;
-                        if (content.Contains("-langVersion:") == false)
-                            newContent = $"{newContent.Trim()} -langVersion:preview";
-
-                        if (content.Contains("-nullable") == false)
-                            newContent = $"{newContent.Trim()} -nullable";
-
-                        if (content.Contains("-doc:") == false)
-                            newContent = $"{newContent.Trim()} -doc:Library/StreamingAssets/{m.Groups[1]}.xml";
-
-                        if (content.Contains("-nowarn:") == false)
-                            newContent = $"{newContent.Trim()} -nowarn:1591";
-
-                        if (content == newContent)
-                            continue;
-
-                        File.WriteAllText(csc, newContent);
-
-                        if (File.Exists(cscMeta) == false)
-                            File.WriteAllText(cscMeta, @$"fileFormatVersion: 2
-guid: {Guid.NewGuid().ToString().Replace("-", "")}
-AssemblyDefinitionImporter:
-  externalObjects: {{}}
-  userData:
-  assetBundleName:
-  assetBundleVariant:
-");
                     }
                 }
                 catch (Exception e)
